9e46e00e99f58a6e23a011fb73c6a2dbed5d3ec0
Miguel Urena <urena_miguel82@outlook.com>
2022-06-18 15:10:01 -0700

    Initial commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void TestMethod() {
+        System.out.println("Holy cannoli! I'm running a method!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world (again)!");
+
+        HelloWorld.TestMethod();
+    }
+}
